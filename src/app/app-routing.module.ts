78e38f801681137f2542c0e0d5d93347a2051bc5
ClaudiaSifuentes <clau2210leti@gmail.com>
2024-06-12 16:28:17 -0500

    fix: Arreglo de ruta en app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: 'gestion-cuenta', loadChildren: () => import('./gestion-cuenta/gestion-cuenta.module').then(m => m.GestionCuentaModule) }, 
   { path: 'gestion-cuenta/register', loadChildren: () => import('./gestion-cuenta/register/register.module').then(m => m.RegisterModule) }, 
   { path: 'gestion-cuenta/login', loadChildren: () => import('./gestion-cuenta/login/login.module').then(m => m.LoginModule) }, 
-  { path: 'gestion-cuenta/forget_password', loadChildren: () => import('./gestion-cuenta/forget-password/forget-password.module').then(m => m.ForgetPasswordModule) }
+  { path: 'gestion-cuenta/forget_password', loadChildren: () => import('./gestion-cuenta/login/forget-password/forget-password.module').then(m => m.ForgetPasswordModule) }
 ];
 
 @NgModule({
