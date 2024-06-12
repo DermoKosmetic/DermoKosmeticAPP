9a95d34dbc6b6e17e2000f440ce8a7c6ffce3276
ClaudiaSifuentes <clau2210leti@gmail.com>
2024-06-12 00:08:14 -0500

    feat: Se agrega Login y parcial forget_password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,  
     MaterialModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/gestion-cuenta/login/forget-password/forget-password.module.ts
+++ src/app/gestion-cuenta/login/forget-password/forget-password.module.ts
@@ -3,19 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ForgetPasswordRoutingModule } from './forget-password-routing.module';
 import { ForgetPasswordComponent } from './forget-password.component';
-import { HomeComponent } from './home/home.component';
-import { InsertEmailComponent } from './insert-email/insert-email.component';
 import { InsertCodeComponent } from './insert-code/insert-code.component';
+import { InsertPasswordComponent } from './insert-password/insert-password.component';
 import { MessageComponent } from './message/message.component';
 
 
 @NgModule({
   declarations: [
     ForgetPasswordComponent,
-    HomeComponent,
-    InsertEmailComponent,
     InsertCodeComponent,
-    MessageComponent
+    InsertPasswordComponent,
+    MessageComponent,
   ],
   imports: [
     CommonModule,
